ec73a623d4be759320064e5d5859cb9dddbbaac6
bijou84 <60628772+bijou84@users.noreply.github.com>
2020-04-16 16:49:07 -0300

    Add files via upload

--- Gustavo Rosa TP1 Calculadora/funmat.h
+++ Gustavo Rosa TP1 Calculadora/funmat.h
@@ -1,9 +1,16 @@
 #ifndef FUNMAT_H_INCLUDED
 #define FUNMAT_H_INCLUDED
 
+
+/** \brief Muestra un menu de opciones al usuario.
+ *
+ * \return Devuelve la opcion seleccionada por el usuario.
+ *
+ */
+
 char menu();
 
-/** \brief Solicita que el usuario ingrese un numero entero
+/** \brief Solicita que el usuario ingrese un numero entero.
  *
  * \return Devuelve el numero entero solicitado.
  *
@@ -45,11 +52,14 @@ int multiplicar(int numero1, int numero2);
  */
 float dividir(float numero1, float numero2);
 
+/** \brief Calcula el factorial de un numero entero ingresado por el usuario
+ *
+ * \param Numero entero ingresado por el usuario.
+ * \return Devuelve el resultado del factorial del numero ingresado por el usuario.
+ *
+ */
+
 int factorial(int numero);
-/* @brief	Nos permite obtener el resultado del factorial de un numero, utilizo for para igualar el contador al numero asi se cuantas veces tiene que dar
-   la vuelta la iteracion, luego si el numero es mas grande que el contador aun, sigo y a medida que da vueltas, va decrementandose el contador.
-* @param	En numeroIngresado, se completa con el numero que ingreso el usuario sea en el caso A o B.
-* @return	Retorna el resultado del factorial de CADA numero.*/
 
 
 #endif // FUNMAT_H_INCLUDED
